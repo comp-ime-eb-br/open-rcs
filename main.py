19f8aa84fa9a88680154a9aacf7432cd1d47ab60
mayribeiro15 <47997138+mayribeiro15@users.noreply.github.com>
2023-09-25 20:43:27 -0300

    handling exceptions

--- main.py
+++ main.py
@@ -181,7 +181,8 @@ class App(customtkinter.CTk):
             
             self.results_window()
             self.monoerror.configure(text="")
-        except:
+        except Exception as e:
+            print(f"An error occurred: {str(e)}")
             self.monoerror.configure(text="Entradas Inválidas!")
             
     def generate_monoresultsfile_event(self):
@@ -208,7 +209,8 @@ class App(customtkinter.CTk):
             
             self.results_window()
             self.monoerror.configure(text="")
-        except:
+        except Exception as e:
+            print(f"An error occurred: {str(e)}")
             self.monoerror.configure(text="Entradas Inválidas!")
             
     def generate_biresults_event(self):
@@ -240,7 +242,8 @@ class App(customtkinter.CTk):
             
             self.results_window()
             self.bierror.configure(text="")
-        except:
+        except Exception as e:
+            print(f"An error occurred: {str(e)}")
             self.bierror.configure(text="Entradas Inválidas!")
 
     def generate_biresultsfile_event(self):
@@ -267,7 +270,8 @@ class App(customtkinter.CTk):
             
             self.results_window()
             self.bierror.configure(text="")
-        except:
+        except Exception as e:
+            print(f"An error occurred: {str(e)}")
             self.bierror.configure(text="Entradas Inválidas!")
 
     def results_window(self):
@@ -333,7 +337,7 @@ class App(customtkinter.CTk):
             plt.show()
             image.close()
         except FileNotFoundError:
-            print(f"The file '{relative_path}' was not found.")
+            print(f"The file '{file_path}' was not found.")
         except Exception as e:
             print(f"An error occurred: {str(e)}")
             
